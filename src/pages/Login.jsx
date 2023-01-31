8320effb5618cea5323ee427cf89845a19f3b9a3
@yla <@yla@DESKTOP-FOSSDE8>
2023-01-31 15:16:37 +0200

    fixed names

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Ayla Chat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
